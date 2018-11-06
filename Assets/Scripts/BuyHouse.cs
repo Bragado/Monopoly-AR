3af22a5b8234b204f043c41c741d39aa1b4cccfb
Bragado <luis_coelho_feita@hotmail.com>
2018-11-06 15:39:27 +0000

    houses in the board

---
+++ Assets/Scripts/BuyHouse.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyHouse : MonoBehaviour {
+
+    public GameObject house1;
+    public GameObject house2;
+    public GameObject house3;
+    public GameObject house4;
+
+    public GameObject hotel;
+
+    private int counter = 0;
+
+    public void addHouse()
+    {
+        switch(counter)
+        {
+            case 0:
+                house1.SetActive(true);
+                break;
+            case 1:
+                house2.SetActive(true);
+                break;
+            case 2:
+                house3.SetActive(true);
+                break;
+            case 3:
+                house4.SetActive(true);
+                break;
+        }
+        counter++;
+
+    }
+
+    public void addHotel()
+    {
+        house1.SetActive(false);
+        house2.SetActive(false);
+        house3.SetActive(false);
+        house4.SetActive(false);
+
+        hotel.SetActive(true);
+    }
+
+
+
+}
---
+++ Assets/Scripts/BuyHousesManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class BuyHousesManager : MonoBehaviour, ITrackableEventHandler
+{
+
+    // whether should buy a house or an hotel
+    public bool buyHouse = true;
+
+
+    public bool startCounting = false;
+    public float timeHolding = 2.5f;
+    public GameObject propertyHouses = null;
+
+
+    private float time = 0.0f;
+    
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+             newStatus == TrackableBehaviour.Status.TRACKED ||
+             newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            startCounting = true;
+           
+        }else
+        {
+            startCounting = false;
+            time = 0.0f;
+        }
+
+    }
+
+   
+	// Update is called once per frame
+	void Update () {
+
+        if(startCounting)
+        {
+            time += Time.deltaTime;
+
+            if(time > timeHolding)
+            {
+
+                // carta identificada
+                // BuyHouse buyHouse = propertyHouses.GetComponent<BuyHouse>();
+                // if(buyHouse) buyhouses.addHouse(); else buyHouses.addHotel();
+                //
+                startCounting = false;
+            }
+
+        }
+        
+
+
+    }
+
+    
+
+
+}
